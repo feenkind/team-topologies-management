0011ef49669335428f3edc597f59254af178683e
Veronika Kustermann <mail@vk-art.de>
2022-10-23 18:16:45 +0200

    Add docker for deployment

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! This endpoint is not implemented.';
+    return 'Hello world! This endpoint is not implemented.';
   }
 }
