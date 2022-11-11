5a9c5bf54166f7ab071c2cf768593a78419ecb67
Veronika Kustermann <mail@vk-art.de>
2022-11-11 19:35:39 +0100

    Display high cognitive load in team view

--- frontend/src/container/Team/TeamViewCognitiveLoad.tsx
+++ frontend/src/container/Team/TeamViewCognitiveLoad.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ITeam } from '../../store/slices/teamSlice';
 import InformationGrid from '../../components/Layout/InformationGrid';
-import { Box, Paper, Tooltip, Typography } from '@mui/material';
+import { Alert, Box, Paper, Tooltip, Typography } from '@mui/material';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import { grey } from '@mui/material/colors';
 import Table from '../../components/Table/Table';
@@ -9,6 +9,7 @@ import ComplexityCategory from '../../components/Categories/ComplexityCategory';
 import { complexity } from '../../constants/categories';
 import { useAppSelector } from '../../hooks';
 import ButtonLink from '../../components/Buttons/ButtonLink';
+import { isCognitiveLoadTooHigh } from './cognitiveLoadFunctions';
 
 interface ITeamViewCognitiveLoadProps {
   team: ITeam;
@@ -74,6 +75,18 @@ const TeamViewCognitiveLoad: React.FC<ITeamViewCognitiveLoadProps> = ({
 
   return (
     <>
+      {isCognitiveLoadTooHigh({
+        cognitiveLoadPoints: team.cognitiveLoad,
+        amountSimpleDomains: domainLinksSimple.length,
+        amountComplicatedDomains: domainLinksComplicated.length,
+        amountComplexDomains: domainLinksComplex.length,
+      }) && (
+        <Alert severity="error" sx={{ mb: 3 }}>
+          Team {team.name} has a high cognitive load. The reasons can be either
+          a very high subjective cognitive load or the responsibility for too
+          many or too complex domains.
+        </Alert>
+      )}
       <InformationGrid
         informationItems={[
           {
@@ -81,11 +94,11 @@ const TeamViewCognitiveLoad: React.FC<ITeamViewCognitiveLoadProps> = ({
             content: team.fte,
           },
           {
-            label: 'Cognitive load',
+            label: 'Subjective cognitive load',
             content: (
               <Box display="flex" alignItems="center">
                 {team.cognitiveLoad}
-                <Tooltip title="Subjective estimation according to the cognitive load assessment">
+                <Tooltip title="Subjective estimation according to the cognitive load assessment. 5 is a very high cognitive load, 25 is a very low cognitive load.">
                   <HelpOutlineOutlinedIcon
                     fontSize="small"
                     sx={{ ml: 1, color: grey[400] }}
