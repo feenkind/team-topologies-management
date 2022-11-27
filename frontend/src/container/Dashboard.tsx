ec599422b4c95eb0c44974fb73642216e7119636
Veronika Kustermann <mail@vk-art.de>
2022-11-27 17:28:00 +0100

    Add link to project list on dashboard

--- frontend/src/container/Dashboard.tsx
+++ frontend/src/container/Dashboard.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
   return (
     <>
-      <Typography variant="body2" component="div">
-        Hello, this is your dashboard. It is still very empty.
+      <Typography variant="body2" component="div" sx={{ my: 5 }}>
+        Hello, this is your dashboard. It is still very empty. Please start with
+        the project list in the meantime.
       </Typography>
+      <Button component={Link} to="/projects" variant="contained">
+        Go to project list
+      </Button>
     </>
   );
 };
