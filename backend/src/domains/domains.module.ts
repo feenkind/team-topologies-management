339c65d8f952cfae8694db08aa9bf77f9d7b7818
Veronika Kustermann <mail@vk-art.de>
2022-12-01 20:41:37 +0100

    Add uml for deployment diagram

--- backend/src/domains/domains.module.ts
+++ backend/src/domains/domains.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { DomainsService } from './domains.service';
 import { DomainsController } from './domains.controller';
 import { PrismaService } from '../prisma.service';
-import { ProjectsService } from '../projects/projects.service';
 
 @Module({
   controllers: [DomainsController],
-  providers: [DomainsService, PrismaService, ProjectsService],
+  providers: [DomainsService, PrismaService],
 })
 export class DomainsModule {}
